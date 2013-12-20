49a1eb3fcc9aff2969e0bc07bcdc0a1f6110081f
xin-jin <xin.s.jin@gmail.com>
2013-12-19 18:33:44 -0800

    preview scale

--- init.el
+++ init.el
@@ -3,12 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(TeX-view-program-selection (quote (((output-dvi style-pstricks) "Evince") (output-dvi "Evince") (output-pdf "Evince") (output-html "Evince"))) t)
- '(custom-safe-themes (quote ("8eef22cd6c122530722104b7c82bc8cdbb690a4ccdd95c5ceec4f3efa5d654f5" "2b5aa66b7d5be41b18cc67f3286ae664134b95ccc4a86c9339c886dfd736132d" default)))
+ '(TeX-view-program-list (quote (("EvinceDbus" auctex-evince-view))))
+ '(custom-safe-themes (quote ("f3ceb7a30f6501c1093bc8ffdf755fe5ddff3a85437deebf3ee8d7bed8991711" "8eef22cd6c122530722104b7c82bc8cdbb690a4ccdd95c5ceec4f3efa5d654f5" "2b5aa66b7d5be41b18cc67f3286ae664134b95ccc4a86c9339c886dfd736132d" default)))
  '(initial-buffer-choice nil)
  '(initial-scratch-message nil)
  '(mode-require-final-newline nil)
  '(paren-highlight-offscreen t)
+ '(preview-scale-function 1.5)
  '(require-final-newline nil))
 
 (scroll-bar-mode 0)
