9ebc0b5ca05b2b1bdeab041fd0e5669f0be70f56
xin-jin <xin.s.jin@gmail.com>
2013-12-28 23:50:15 -0800

    auctex

--- init.el
+++ init.el
@@ -112,8 +112,8 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-(load "auctex.el" nil t t)
-(load "preview-latex.el" nil t t)
+; (load "auctex.el" nil t t)
+; (load "preview-latex.el" nil t t)
 
 (setq TeX-auto-save t)
 (setq TeX-parse-self t)
