92761428409079105f13bb739583bf152d8dc2eb
xin-jin <xin.s.jin@gmail.com>
2014-01-02 19:23:36 -0800

    xin-cyber theme

--- elpa/zen-and-art-theme-20120622.737/zen-and-art-theme-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; zen-and-art-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "zen-and-art-theme" "zen-and-art-theme.el"
-;;;;;;  (21177 10138 970299 636000))
-;;; Generated autoloads from zen-and-art-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("zen-and-art-theme-pkg.el") (21177 10139
-;;;;;;  1438 935000))
-
-;;;***
-
-(provide 'zen-and-art-theme-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; zen-and-art-theme-autoloads.el ends here
