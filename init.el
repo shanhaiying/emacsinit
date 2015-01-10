0854be655c1477f01c2c979b20e26f21d5e3d869
xin-jin <xin.s.jin@gmail.com>
2015-01-10 13:08:30 -0800

    coding system

--- init.el
+++ init.el
@@ -35,6 +35,16 @@
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 
+(setq utf-translate-cjk-mode nil) ; disable CJK coding/encoding (Chinese/Japanese/Korean characters)
+(set-language-environment 'utf-8)
+(set-keyboard-coding-system 'utf-8-unix) ; For old Carbon emacs on OS X only
+(setq locale-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(unless (eq system-type 'windows-nt)
+  (set-selection-coding-system 'utf-8))
+(prefer-coding-system 'utf-8)
+
 
 ;;--------------------------------------------------------------------------------------------------------------------------------------------
 ;; ---- BEGIN ENVIRONMENT VARIABLES ----------------------------------------------------------------------------------------------------------
